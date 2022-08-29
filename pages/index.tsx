09e10993f3206d68561847c0da1b36e4d06440d5
Jarrod Watts <35651410+jarrodwatts@users.noreply.github.com>
2022-08-29 12:48:35 +1000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,8 @@
-import { useAddress, useDisconnect, useMetamask } from '@thirdweb-dev/react';
+import { ConnectWallet } from "@thirdweb-dev/react";
 import type { NextPage } from 'next';
 
-const Home: NextPage = () => {
-  const address = useAddress();
-  const connectWithMetamask = useMetamask();
-  const disconnectWallet = useDisconnect();
+export default function Home(): NextPage {
   return (
-    <div>
-      {address ? (
-        <>
-          <button onClick={disconnectWallet}>Disconnect Wallet</button>
-          <p>Your address: {address}</p>
-        </>
-      ) : (
-        <button onClick={connectWithMetamask}>Connect with Metamask</button>
-      )}
-    </div>
+      <ConnectWallet accentColor="#f213a4" colorMode="light" />
   );
-};
-
-export default Home;
+}
