3307b4e204cec6073802bb9289ca872d5c3ddaf1
Jarrod Watts <jarrodwatts16@gmail.com>
2022-09-28 14:40:37 +1000

    Add styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
-import type { AppProps } from 'next/app';
-import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
+import type { AppProps } from "next/app";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import "../styles/globals.css";
 
 // This is the chainId your dApp will work on.
 const activeChainId = ChainId.Mainnet;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,53 @@
 import { ConnectWallet } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <ConnectWallet accentColor="#f213a4" colorMode="light" />
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">thirdweb</a>!
+        </h1>
+
+        <p className={styles.description}>
+          Get started by configuring your desired network in{" "}
+          <code className={styles.code}>pages/_app.js</code>, then modify the{" "}
+          <code className={styles.code}>pages/index.js</code> file!
+        </p>
+
+        <div className={styles.connect}>
+          <ConnectWallet />
+        </div>
+
+        <div className={styles.grid}>
+          <a href="https://portal.thirdweb.com/" className={styles.card}>
+            <h2>Portal &rarr;</h2>
+            <p>
+              Guides, references and resources that will help you build with
+              thirdweb.
+            </p>
+          </a>
+
+          <a href="https://thirdweb.com/dashboard" className={styles.card}>
+            <h2>Dashboard &rarr;</h2>
+            <p>
+              Deploy, configure and manage your smart contracts from the
+              dashboard.
+            </p>
+          </a>
+
+          <a
+            href="https://portal.thirdweb.com/templates"
+            className={styles.card}
+          >
+            <h2>Templates &rarr;</h2>
+            <p>
+              Discover and clone template projects showcasing thirdweb features.
+            </p>
+          </a>
+        </div>
+      </main>
     </div>
   );
 };
