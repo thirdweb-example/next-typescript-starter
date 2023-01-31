40065630d9552035d8fb64f5bccf48292e590b4e
lakshayZebpay <107855591+lakshayZebpay@users.noreply.github.com>
2023-01-31 18:37:56 +0530

    Verify flow (#8)
    * kyc flow
    
    * merge conflict
    
    * verify flow

--- components/SideBar/typings.ts
+++ components/SideBar/typings.ts
@@ -1,4 +1,4 @@
-export type SideBarTabs = "profile" | "docs" | "setting";
+export type SideBarTabs = "profile" | "docs" | "setting" | "verify";
 export interface SideBarProps {
   expand: boolean;
   toggleExpand: () => void;
--- pages/verify.tsx
+++ pages/verify.tsx
@@ -1,5 +1,7 @@
 /** @jsxImportSource @emotion/react */
 
+import { UploadOutlined } from "@ant-design/icons";
+import { Button, Upload, UploadFile } from "antd";
 import type { NextPage } from "next";
 import { useEffect, useState } from "react";
 import Footer from "../components/Footer";
@@ -7,23 +9,37 @@ import Header from "../components/Header";
 import SideBar from "../components/SideBar";
 import UserProfile from "../components/UserProfile";
 
+
 const Verify: NextPage = () => {
   const [openSideDrawer, setOpenSideDrawer] = useState<boolean>(false);
-
+    const [uploadedFile,setUploadedFIle] =useState<UploadFile[]>();
     useEffect(()=>{
         //call user api to check if logged in
         //call api for stored document when user is successful
 
     })
+const handleUpload= ()=>{
+
+}
   return (
     <div style={{height:"100vh", background:"#F5F5F5"}}>
        
        
         <div css={{display:"flex"}}>
-        <SideBar expand={openSideDrawer} toggleExpand={()=>{setOpenSideDrawer(prev=>!prev)}} selected="profile"/>
+        <SideBar expand={openSideDrawer} toggleExpand={()=>{setOpenSideDrawer(prev=>!prev)}} selected="verify"/>
         <div css={{width:openSideDrawer? "90%":"95%",justifyContent:"end", transition:"0.2s all ease-in",marginLeft:"auto"}}>
           <Header />
-          <UserProfile />
+          <Upload
+            listType="picture-card"
+            // defaultFileList={[...fileList]}
+            onChange={(info)=>{
+                setUploadedFIle(info.fileList);
+            }}
+            >
+                <Button icon={<UploadOutlined /> }>Upload</Button>
+            </Upload>
+            <Button onClick={handleUpload}>Verify</Button>
+            
         </div>
         </div>
         <Footer />
