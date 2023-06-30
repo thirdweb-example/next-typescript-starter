27e806dfd5b01fb38e6b299c4af6b60a6ecb233d
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2023-06-30 14:13:57 +0530

    fix: build

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { ConnectWallet } from "@thirdweb-dev/react";
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
+import { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
