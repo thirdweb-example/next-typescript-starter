bb5db9930609606e80c50d7a3f4fbf2afbc3c2d1
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2023-06-30 14:04:15 +0530

    fix: add types

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import { ConnectWallet } from "@thirdweb-dev/react";
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
 
-export default function Home() {
+const Home: NextPage = () => {
   return (
     <main className={styles.main}>
       <div className={styles.container}>
@@ -103,4 +103,6 @@ export default function Home() {
       </div>
     </main>
   );
-}
+};
+
+export default Home;
