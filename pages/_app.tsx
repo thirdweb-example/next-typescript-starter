af748e14a852d6def7f2531f27613734f9914efb
Manan Tank <manantankm@gmail.com>
2023-07-25 01:59:37 +0530

    Add client id

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,21 @@
-import type { AppProps } from "next/app";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
-import "../styles/globals.css";
+import type { AppProps } from 'next/app';
+import { ThirdwebProvider } from '@thirdweb-dev/react';
+import '../styles/globals.css';
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+const activeChain = 'ethereum';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ThirdwebProvider activeChain={activeChain}>
-      <Component {...pageProps} />
-    </ThirdwebProvider>
-  );
+	return (
+		<ThirdwebProvider
+			activeChain={activeChain}
+			clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
+		>
+			<Component {...pageProps} />
+		</ThirdwebProvider>
+	);
 }
 
 export default MyApp;
