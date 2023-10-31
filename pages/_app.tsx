0901c860ed6653fd3d24f0d9301207dd24952415
Manan Tank <manantankm@gmail.com>
2023-10-31 15:16:34 +0530

    use mumbai

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,13 +11,12 @@ import {
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = 'ethereum';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<ThirdwebProvider
 			clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
-			activeChain={activeChain}
+			activeChain={'mumbai'}
 			supportedWallets={[
 				comethConnect({
 					// apiKey: 'bef322c7-2612-4f91-ab1d-65c99c88c758',
