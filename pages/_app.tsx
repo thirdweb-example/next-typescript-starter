86c8fe1806b16faf2d948776e619c1b91ed155f3
Manan Tank <manantankm@gmail.com>
2023-10-31 15:12:30 +0530

    [test] cometh connect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,36 @@
-import type { AppProps } from "next/app";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
-import "../styles/globals.css";
+import type { AppProps } from 'next/app';
+import '../styles/globals.css';
+import {
+	ThirdwebProvider,
+	coinbaseWallet,
+	metamaskWallet,
+	comethConnect,
+	walletConnect,
+} from '@thirdweb-dev/react';
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+const activeChain = 'ethereum';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ThirdwebProvider
-      clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
-      activeChain={activeChain}
-    >
-      <Component {...pageProps} />
-    </ThirdwebProvider>
-  );
+	return (
+		<ThirdwebProvider
+			clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
+			activeChain={activeChain}
+			supportedWallets={[
+				comethConnect({
+					// apiKey: 'bef322c7-2612-4f91-ab1d-65c99c88c758',
+					apiKey: '4153e44d-5af6-472e-86f5-4447ad6b8d12', // polygon mumbai
+				}),
+				metamaskWallet(),
+				coinbaseWallet(),
+				walletConnect(),
+			]}
+		>
+			<Component {...pageProps} />
+		</ThirdwebProvider>
+	);
 }
 
 export default MyApp;
