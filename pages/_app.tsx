1505dde2e0877253745e0a8af767e8e6c875fd2a
ikethirdweb <jorge@thirdweb.com>
2023-11-20 16:54:40 -0500

    testing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,17 @@
 import type { AppProps } from "next/app";
 import { ThirdwebProvider, embeddedWallet } from "@thirdweb-dev/react";
 import "../styles/globals.css";
-import { Sepolia } from "@thirdweb-dev/chains";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = Sepolia;
+const activeChain = "ethereum";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
       clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
       activeChain={activeChain}
-      supportedChains={[activeChain]}
       supportedWallets={[
         embeddedWallet({
           auth: {
